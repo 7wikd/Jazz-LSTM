732927b225476b10f6986046ab8f09f9f962d18b
7wikd <satwik.dalvi@gmail.com>
2024-06-08 11:41:13 +0100

    Fix

--- generate.py
+++ generate.py
@@ -1,25 +1,109 @@
 '''
-Generating music 
+generate.py
+1. The generate() function reads the notes from the notes file using the pickle module.
+2. The sequences() function creates sequences of notes from the notes extracted in the generate() function. It then converts the notes to integers and normalizes them.
+3. The create_net() function creates a neural network model using the Keras Sequential API. The model consists of three LSTM layers, two Dropout layers, two BatchNormalization layers, and two Dense layers.
+4. The generate_notes() function generates notes using the model, sequences, pitchnames, and n_vocab.
+5. The create_midi() function creates a MIDI file using the notes generated in the generate_notes() function.
+6. The if __name__ == '__main__': block calls the generate() function when the script is run.
 '''
-
 import pickle
 import numpy as np
 
-from music21 import instrument,note, stream, chord
+from music21 import instrument, note, stream, chord
 
 from keras.models import Sequential
 from keras.layers import Activation, BatchNormalization, Dense, Dropout, LSTM
+from keras.utils import to_categorical
 
 def generate():
-    with open('misc/notes','rb') as filepath:
+    with open('misc/notes', 'rb') as filepath:
         notes = pickle.load(filepath)
 
-        pitchnames = sorted(set(item for item in notes))
-        n_vocab = len(set(notes))
+    pitchnames = sorted(set(item for item in notes))
+    n_vocab = len(set(notes))
+    net_in, _ = sequences(notes, pitchnames, n_vocab)
+    model = create_net(net_in, n_vocab)
+    model.load_weights('./model_first.keras')
+    prediction_output = generate_notes(model, net_in, pitchnames, n_vocab)
+    create_midi(prediction_output)
 
-        net_in,norm_out = sequences(notes, pitchnames, n_vocab)
-        model = net(norm_in,n_vocab)
-        pred_op = generate_notes(model, net_in, pitchnames, n_vocab)
-        create_midi(pred_op)
+def sequences(notes, pitchnames, n_vocab):
+    seq_len = 100
+    note_to_int = {note: num for num, note in enumerate(pitchnames)}
+    net_in = []
+    net_out = []
 
-def sequences(notes,pitchnames,n_vocab):
+    for i in range(0, len(notes) - seq_len, 1):
+        in_seq = notes[i:i + seq_len]
+        out_seq = notes[i + seq_len]
+        net_in.append([note_to_int[char] for char in in_seq])
+        net_out.append(note_to_int[out_seq])
+
+    n_patterns = len(net_in)
+    net_in = np.reshape(net_in, (n_patterns, seq_len, 1))
+    net_in = net_in / float(n_vocab)
+    net_out = to_categorical(net_out)
+
+    return net_in, net_out
+
+def create_net(net_in, n_vocab):
+    model = Sequential([
+        LSTM(512, input_shape=(net_in.shape[1], net_in.shape[2]), recurrent_dropout=0.4, return_sequences=True),
+        LSTM(512, return_sequences=True, recurrent_dropout=0.4),
+        LSTM(512),
+        BatchNormalization(),
+        Dropout(0.5),
+        Dense(256),
+        Activation('relu'),
+        BatchNormalization(),
+        Dropout(0.2),
+        Dense(n_vocab),
+        Activation('softmax')
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    return model
+
+def generate_notes(model, net_in, pitchnames, n_vocab):
+    start = np.random.randint(0, len(net_in) - 1)
+    int_to_note = {num: note for num, note in enumerate(pitchnames)}
+    pattern = net_in[start]
+    prediction_output = []
+
+    for note_index in range(500):
+        prediction_input = np.reshape(pattern, (1, len(pattern), 1))
+        prediction_input = prediction_input / float(n_vocab)
+        prediction = model.predict(prediction_input, verbose=0)
+        index = np.argmax(prediction)
+        result = int_to_note[index]
+        prediction_output.append(result)
+        pattern = np.append(pattern, index)
+        pattern = pattern[1:len(pattern)]
+
+    return prediction_output
+
+def create_midi(prediction_output):
+    offset = 0
+    output_notes = []
+
+    for pattern in prediction_output:
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = [note.Note(int(current_note)) for current_note in notes_in_chord]
+            for new_note in notes:
+                new_note.storedInstrument = instrument.Piano()
+            new_chord = chord.Chord(notes)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+        else:
+            new_note = note.Note(pattern)
+            new_note.offset = offset
+            new_note.storedInstrument = instrument.Piano()
+            output_notes.append(new_note)
+        offset += 0.5
+
+    midi_stream = stream.Stream(output_notes)
+    midi_stream.write('midi', fp='output.mid')
+
+if __name__ == '__main__':
+    generate()
--- prepare.py
+++ prepare.py
@@ -1,36 +1,40 @@
 '''
-Preparing the model 
+prepare.py
+1. The get_notes() function reads all the MIDI files in the midi directory and extracts the notes and chords from them. It then saves the notes in a file named notes using the pickle module.
+2. The sequence() function creates sequences of notes from the notes extracted in the get_notes() function. It then converts the notes to integers and normalizes them.
+3. The create_net() function creates a neural network model using the Keras Sequential API. The model consists of three LSTM layers, two Dropout layers, two BatchNormalization layers, and two Dense layers.
+4. The net() function trains the model using the net_input and net_output sequences. It saves the best model using the ModelCheckpoint callback.
+5. The train() function calls the get_notes() function to extract the notes from the MIDI files, creates sequences of notes using the sequence() function, creates a neural network model using the create_net() function, and trains the model using the net() function.
+6. The if __name__ == '__main__': block calls the train() function when the script is run.
 '''
 
 import glob
 import pickle
 import numpy as np
+import os
 
 from music21 import converter, instrument, note, chord
 
 from keras.layers import Activation, BatchNormalization, Dense, Dropout, LSTM
 from keras.optimizers import Adam
 from keras.models import Sequential
-from keras.utils import np_utils
+from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 def train():
     notes = get_notes()
-
     n_vocab = len(set(notes))
-
     net_input, net_output = sequence(notes, n_vocab)
-
     model = create_net(net_input, n_vocab)
-
     net(model, net_input, net_output)
 
 def get_notes():
     notes = []
     for file in glob.glob('midi/*.mid'):
         midi = converter.parse(file)
-        print("Parsing: %s " %file)
-
+        print(f"Parsing: {file}")
         notes_to_parse = None
 
         try:
@@ -39,56 +43,41 @@ def get_notes():
         except:
             notes_to_parse = midi.flat.notes
 
-        
         for element in notes_to_parse:
-            if isinstance(element,note.Note):
+            if isinstance(element, note.Note):
                 notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):
                 notes.append('.'.join(str(n) for n in element.normalOrder))
-    
-    with open('misc/notes','wb') as filepath:
+
+    with open('misc/notes', 'wb') as filepath:
         pickle.dump(notes, filepath)
 
     return notes
 
 def sequence(notes, n_vocab):
     seq_len = 100
-    
     pitchnames = sorted(set(item for item in notes))
-
-    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+    note_to_int = {note: number for number, note in enumerate(pitchnames)}
     net_in = []
     net_out = []
 
-    for i in range(0,len(notes)-seq_len,1):
-        seq_in = notes[i: i+seq_len]
-        seq_out = notes[i+seq_len]
-        
+    for i in range(0, len(notes) - seq_len, 1):
+        seq_in = notes[i: i + seq_len]
+        seq_out = notes[i + seq_len]
         net_in.append([note_to_int[char] for char in seq_in])
         net_out.append(note_to_int[seq_out])
-    
+
     n_patterns = len(net_in)
+    net_in = np.reshape(net_in, (n_patterns, seq_len, 1))
+    net_in = net_in / float(n_vocab)
+    net_out = to_categorical(net_out)
 
-    net_in = np.reshape(net_in,(n_patterns,seq_len,1))
-    net_in = net_in /float(n_vocab)
-
-    net_out = np_utils.to_categorical(net_out)
-
-    return (net_in,net_out)
+    return net_in, net_out
 
 def create_net(net_in, n_vocab):
     model = Sequential([
-        LSTM(
-            512,
-            input_shape = (net_in.shape[1],net_in.shape[2]),
-            recurrent_dropout=0.4,
-            return_sequences=True
-        ),
-        LSTM(
-            512, 
-            return_sequences=True, 
-            recurrent_dropout=0.4
-        ),
+        LSTM(512, input_shape=(net_in.shape[1], net_in.shape[2]), recurrent_dropout=0.4, return_sequences=True),
+        LSTM(512, return_sequences=True, recurrent_dropout=0.4),
         LSTM(512),
         BatchNormalization(),
         Dropout(0.5),
@@ -99,21 +88,14 @@ def create_net(net_in, n_vocab):
         Dense(n_vocab),
         Activation('softmax')
     ])
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.01))
-    
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01))
     return model
 
-def net(model, net_in,net_out):
-    filepath = 'model_first.hdf5'
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
-    )
+def net(model, net_in, net_out):
+    filepath = 'model_first.keras'
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
-    model.fit(net_in,net_out,epochs=200,batch_size=64,callbacks=callbacks_list)
-  
+    model.fit(net_in, net_out, epochs=200, batch_size=64, callbacks=callbacks_list)
+
 if __name__ == '__main__':
     train()
